25f4d8ea6148bf95ea84d5836775d369a2dd5871
o0cynix0o <o0cynix0o@gmail.com>
2024-05-07 18:12:52 -0600

    Update Emby Restart.ps1

--- Emby Restart.ps1
+++ Emby Restart.ps1
@@ -1,4 +1,15 @@
+#Use USERNAME to set the name of the user emby is installed as.
+
+#Stopping Emby processes.
+
 Stop-Process -Name "EmbyServer"
+
 Stop-Process -Name "embytray"
+
+#Pause for prelocess to close. 
+
 Start-Sleep -Seconds 5
+
+#Start Emby
+
 Start-Process -FilePath "C:\Users\USERNAME\AppData\Roaming\Emby-Server\system\EmbyServer.exe"
