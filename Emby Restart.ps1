883b2708751266cc628373dc89acb2692459163d
o0cynix0o <o0cynix0o@gmail.com>
2024-05-08 08:32:04 -0600

    Update Emby Restart.ps1

--- Emby Restart.ps1
+++ Emby Restart.ps1
@@ -1,5 +1,11 @@
 #Use USERNAME to set the name of the user emby is installed as.
 
+<<<<<<< Updated upstream
+=======
+#Stop Emby processes
+
+Write-Host "Stopping Emby services, please stand by."
+>>>>>>> Stashed changes
 
 #Stopping the running Emby processes.
 
@@ -7,11 +13,23 @@ Write-Host "Stopping Emby Services. Please stand by."
 Stop-Process -Name "EmbyServer"
 Stop-Process -Name "embytray"
 
-#Pause for process to close. 
+#Pause for process to close 
+
+Start-Sleep -Seconds 1
+
+#Start Emby Services
+
+Write-Host "Starting Emby back up."
+
+Start-Process -FilePath "C:\Users\USERNAME\AppData\Roaming\Emby-Server\system\EmbyServer.exe"
 
 Start-Sleep -Seconds 5
 
-#Start Emby
+Write-Host "Should be good to go now."
 
+<<<<<<< Updated upstream
 Write-Host "Starting Emby back up."
 Start-Process -FilePath "C:\Users\USERNAME\AppData\Roaming\Emby-Server\system\EmbyServer.exe"
+=======
+Exit
+>>>>>>> Stashed changes
